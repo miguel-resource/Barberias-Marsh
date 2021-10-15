0528183681bc45a3395a0910c16a2a4e5b76c1a6
Daniel <dani_wii20@hotmail.com>
2021-10-15 11:12:01 -0500

    Add MaterialModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BannerComponent } from './banner/banner.component';
+import { MaterialModule } from './../material/material.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BannerComponent
+  ],
   imports: [
+    MaterialModule,
     CommonModule
   ]
 })
